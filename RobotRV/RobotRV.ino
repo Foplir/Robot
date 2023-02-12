570cadb5dbe9b97e4940ddcafae75b897adff855
Foplir <104756080+Foplir@users.noreply.github.com>
2023-02-12 19:53:53 +0400

    Add files via upload

--- RobotRV/RobotRV.ino
+++ RobotRV/RobotRV.ino
@@ -1,20 +1,30 @@
-#define byte motorA;
-#define byte motorB;
-#define byte sensorFA;
-#define byte sensorFB;
-#define byte sensorRA;
-#define byte sensorRB;
-#define byte sensorLA;
-#define byte sensorLB;
-#define byte sensorBA;
-#define byte sensorBB;
-#define byte sensorColor;
+/* Устанавливаем значение переменных */
+
+/* Значения пинов */
+byte motorA;
+byte motorB;
+byte sensorFA;
+byte sensorFB;
+byte sensorRA;
+byte sensorRB;
+byte sensorLA;
+byte sensorLB;
+byte sensorBA;
+byte sensorBB;
+byte sensorColor;
+
+/* Остальные значения */
+int durationFA;
+int durationFB;
+int distFA;
+int distFB;
 
 void setup() {
+  //настройка пинов
   pinMode(motorA, OUTPUT);
   pinMode(motorB, OUTPUT);
   pinMode(sensorFA, INPUT);
-  pinMode(sensorFB, INPUT);
+  pinMode(sensorFB, INPUT); 
   pinMode(sensorRA, INPUT);
   pinMode(sensorRB, INPUT);
   pinMode(sensorLA, INPUT);
@@ -22,9 +32,23 @@ void setup() {
   pinMode(sensorBA, INPUT);
   pinMode(sensorBB, INPUT);
   pinMode(sensorColor, INPUT);
-  
+
+  //настройка вывода в консоль
+  Serial.begin(9600);
 }
 
 void loop() {
- 
+  //считывание волны
+  durationFA = pulseIn(sensorFA, HIGH);
+  duratioFB = pulseIn(echo, HIGH);
+  
+  //преобразование значения датчиков в см 
+  distFA = (durationFA/2)/29;
+  distFB = (duratioFB/2)/29;
+
+  //вывод расстояния в консоль
+  Serial.println("{",distFA, "} {", distFB, "}");
+  
+  //задержка для корректной работы датчиков
+  delay(100);
 }
