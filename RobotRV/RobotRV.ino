f6cbde5922ea0b090177d13f1c1ee4471cd16e5f
Foplir <104756080+Foplir@users.noreply.github.com>
2023-02-12 19:31:21 +0400

    1v

---
+++ RobotRV/RobotRV.ino
@@ -0,0 +1,30 @@
+#define byte motorA;
+#define byte motorB;
+#define byte sensorFA;
+#define byte sensorFB;
+#define byte sensorRA;
+#define byte sensorRB;
+#define byte sensorLA;
+#define byte sensorLB;
+#define byte sensorBA;
+#define byte sensorBB;
+#define byte sensorColor;
+
+void setup() {
+  pinMode(motorA, OUTPUT);
+  pinMode(motorB, OUTPUT);
+  pinMode(sensorFA, INPUT);
+  pinMode(sensorFB, INPUT);
+  pinMode(sensorRA, INPUT);
+  pinMode(sensorRB, INPUT);
+  pinMode(sensorLA, INPUT);
+  pinMode(sensorLB, INPUT);
+  pinMode(sensorBA, INPUT);
+  pinMode(sensorBB, INPUT);
+  pinMode(sensorColor, INPUT);
+  
+}
+
+void loop() {
+ 
+}
